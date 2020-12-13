2d323f7eb3f6508bbac18f95ebc9665bc0e58b76
VarSar <varunsarpal271@gmail.com>
2020-12-13 16:05:08 +0530

    changes

--- index.js
+++ index.js
@@ -2,49 +2,54 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '';
 var bot = new TelegramBot(token, {polling: true});
 
-var item = [
-    {Key: "Hi", Value: "Sup!"},
-    {Key: "Name", Value: "Muscular Nerd!"},
+var items = [
+    {Key: "hi", Value: "Sup!"},
+    {Key: "name", Value: "Muscular Nerd!"},
     {Key: "", Value: ""},
     {Key: "", Value: ""},
     {Key: "", Value: ""},
     {Key: "", Value: ""}
 ];
 
+bot.onText(/\/start/, (msg) => {   
+    var chatId = msg.chat.id;
+    // var echo = match[1];
+    var start = `Welcome to Muscular Nerd!!
+    \nYour one stop for technological excellence.
+    \nType /help for more info.
+    `;
+    bot.sendMessage(chatId, start);
+});
+
 bot.onText(/\/help/, (msg) => {   
     var chatId = msg.chat.id;
     // var echo = match[1];
     var help = `Welcome to Muscular Nerd!
     \n"/echo *MSG*" - To Repeat your message
-    \n"/swag *MSG*" - To swag it up
+    \n"/get *MSG*" - To get items
     `;
 
     bot.sendMessage(chatId, help);
 });
 
-bot.onText(/\/start/, (msg) => {   
-    var chatId = msg.chat.id;
-    // var echo = match[1];
-    var help = `Welcome to Muscular Nerd!
-    \n"/echo *MSG*" - To Repeat your message
-    \n"/swag *MSG*" - To swag it up
-    `;
+// bot.onText(/\/echo (.+)/, (msg, match) => {   
+//     var chatId = msg.chat.id;
+//     var echo = match[1];
+//     bot.sendMessage(chatId, echo);
+// });
 
-    bot.sendMessage(chatId, help);
-});
-
-bot.onText(/\/echo (.+)/, (msg, match) => {   
+bot.onText(/\/get (.+)/, (msg, match) => {   
     var chatId = msg.chat.id;
     var echo = match[1];
-    bot.sendMessage(chatId, echo);
-});
+    echo = echo.trim().toLowerCase();
+    let item = items.find(x=> x.Key == echo);
+    let result = item.Value;
 
-bot.onText(/\/swag (.+)/, (msg, match) => {   
-    var chatId = msg.chat.id;
-    var echo = match[1];
-    let result = item.find(x=> x.Key == echo);
+    if (result == null || result == undefined || result == ''){
+        result = "Sorry, didnt get that!! Try something else";
+    }
 
-    bot.sendMessage(chatId, result.Value);
+    bot.sendMessage(chatId, result);
 });
 
 
